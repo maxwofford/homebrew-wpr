2ec2a1022f05a9fc90fb97d9a00e1fcc76a5cc44
Max Wofford <max@maxwofford.com>
2026-09-17 21:27:02 -0400

    bump to version 0.1.2

--- Formula/wpr.rb
+++ Formula/wpr.rb
@@ -1,8 +1,8 @@
 class Wpr < Formula
   desc "Write shaders, use them as your wallpaper."
   homepage "https://github.com/maxwofford/wpr"
-  url "https://github.com/maxwofford/wpr/releases/download/v0.1.1/wpr-v0.1.1-macos.tar.gz"
-  sha256 "47033bfcd4de8edc72828e93d6c3985f5f27a643a3e3ede544b06dd952597772"
+  url "https://github.com/maxwofford/wpr/releases/download/v0.1.2/wpr-v0.1.2-macos.tar.gz"
+  sha256 "20efc01747519ce03e90605555eefd7615e12173aad9a85e17521f09879b2b7c"
   license "MIT"
   head "https://github.com/maxwofford/wpr.git", branch: "main"
 
